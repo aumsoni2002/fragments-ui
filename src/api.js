273ca3365de36e49a426fc88cc292346f518165a
Aum <aumsoni2002@gmail.com>
2024-04-18 13:47:14 -0400

    Assignment 3 Complete

--- src/api.js
+++ src/api.js
@@ -1,5 +1,3 @@
-// src/api.js
-
 // fragments microservice API, defaults to localhost:8080
 const apiUrl = process.env.API_URL || "http://localhost:8080";
 
@@ -66,3 +64,172 @@ export async function createNewFragment(user, fragmentContent, contentType) {
     throw error;
   }
 }
+
+/**
+ * GET /v1/fragments/:id
+ */
+export async function fetchFragmentById(user, fragmentId) {
+  console.log("Fetching user fragment by id...");
+
+  try {
+    const res = await fetch(`${apiUrl}/v1/fragments/${fragmentId}`, {
+      method: "GET",
+      headers: user.authorizationHeaders(),
+    });
+
+    if (!res.ok) {
+      throw await res.json();
+    }
+
+    let data;
+    const fragmentType = res.headers.get("Content-Type");
+
+    if (fragmentType.startsWith("text/")) {
+      data = await res.text();
+    } else if (fragmentType.startsWith("application/")) {
+      data = await res.json();
+    } else if (fragmentType.startsWith("image/")) {
+      const blob = await res.blob();
+      data = URL.createObjectURL(blob);
+    }
+
+    console.log("Success in retrieving fragment data: ", { data });
+
+    return { data, fragmentType };
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
+}
+
+/**
+ * PUT /v1/fragments/:id
+ */
+export async function updateFragmentById(
+  user,
+  content,
+  fragmentId,
+  fragmentType
+) {
+  console.log("Updating fragment by id...");
+
+  try {
+    const res = await fetch(`${apiUrl}/v1/fragments/${fragmentId}`, {
+      method: "PUT",
+      headers: {
+        Authorization: user.authorizationHeaders().Authorization,
+        "Content-Type": fragmentType,
+      },
+      body: content,
+    });
+
+    if (!res.ok) {
+      throw await res.json();
+    }
+
+    const data = await res.json();
+
+    console.log("Success in updating fragment data: ", { data });
+
+    return data;
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
+}
+
+/**
+ * DELETE /v1/fragments/:id
+ */
+export async function deleteFragmentById(user, id) {
+  console.log("deleting fragment by id...");
+
+  return await fetch(`${apiUrl}/v1/fragments/${id}`, {
+    method: "DELETE",
+    headers: {
+      Authorization: user.authorizationHeaders().Authorization,
+    },
+  })
+    .then((res) => {
+      if (!res.ok) {
+        throw res.json();
+      }
+      return res.json();
+    })
+    .then((data) => {
+      console.log("Success in deleting fragment: ", { data });
+      return data;
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+}
+
+/**
+ * Fetch converted fragment by id and extension
+ */
+export async function fetchConvertedFragmentById(user, fragmentId, extension) {
+  const extensionType = getExtensionContentType(extension);
+  if (!extensionType) {
+    throw new Error(`Unsupported extension: ${extension}`);
+  }
+
+  console.log(
+    `Fetching from URL: ${apiUrl}/v1/fragments/${fragmentId}.${extension}`
+  );
+
+  try {
+    const response = await fetch(
+      `${apiUrl}/v1/fragments/${fragmentId}.${extension}`,
+      {
+        method: "GET",
+        headers: {
+          Authorization: user.authorizationHeaders().Authorization,
+          Accept: extensionType,
+        },
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    let data;
+    if (extension.startsWith("image/")) {
+      const blob = await response.blob();
+      data = URL.createObjectURL(blob);
+    } else if (extensionType === "application/json") {
+      data = await response.json();
+    } else {
+      data = await response.text();
+    }
+
+    return { data };
+  } catch (error) {
+    throw new Error(`Error retrieving converted fragment: ${error.message}`);
+  }
+}
+
+// getting extension as per its respective content type
+const getExtensionContentType = (extension) => {
+  switch (extension) {
+    case "txt":
+      return "text/plain";
+    case "md":
+      return "text/markdown";
+    case "html":
+      return "text/html";
+    case "json":
+      return "application/json";
+    case "png":
+      return "image/png";
+    case "jpg":
+      return "image/jpeg";
+    case "webp":
+      return "image/webp";
+    case "gif":
+      return "image/gif";
+    default:
+      return null;
+  }
+};
--- src/app.js
+++ src/app.js
@@ -1,12 +1,32 @@
-// src/app.js
-
 import { Auth, getUser } from "./auth";
-import { fetchUserFragments, createNewFragment } from "./api";
+import {
+  fetchUserFragments,
+  createNewFragment,
+  fetchFragmentById,
+  updateFragmentById,
+  deleteFragmentById,
+  fetchConvertedFragmentById,
+} from "./api";
 
 async function init() {
   const userSection = document.querySelector("#user");
   const loginBtn = document.querySelector("#login");
   const logoutBtn = document.querySelector("#logout");
+  const fragmentList = document.getElementById("fragmentList");
+  const createForm = document.getElementById("createForm");
+  const updateForm = document.getElementById("updateForm");
+  const viewModal = document.getElementById("viewModal");
+  const updateModal = document.getElementById("updateModal");
+  const deleteModal = document.getElementById("deleteModal");
+  const viewFragmentContent = document.getElementById("viewFragmentContent");
+  const updateFragmentContent = document.getElementById(
+    "updateFragmentContent"
+  );
+  const extensionInput = document.getElementById("extensionInput");
+  const convertBtn = document.getElementById("convertBtn");
+  const confirmDelete = document.getElementById("confirmDelete");
+  const imageFragment = document.querySelector("#imageFragment");
+  const updateImageFragment = document.querySelector("#updateImageFragment");
 
   loginBtn.onclick = () => {
     Auth.federatedSignIn();
@@ -30,8 +50,6 @@ async function init() {
   loginBtn.style.display = "none"; // Hide login button if user is logged in
   logoutBtn.style.display = "block"; // Show logout button
 
-  const fragmentList = document.getElementById("fragmentList");
-
   // Function to display fragments
   async function displayFragments(user) {
     fragmentList.innerHTML = ""; // Clear existing fragment list
@@ -48,7 +66,15 @@ async function init() {
                        fragment.updated
                      ).toLocaleString()}</div>
                      <div>Size: ${fragment.size}</div>
-                     <div>Owner ID: ${fragment.ownerId}</div>`;
+                     <div>Owner ID: ${fragment.ownerId}</div>
+                     <div>
+                       <button class="deleteBtn" data-id="${
+                         fragment.id
+                       }">Delete</button>
+                       <button class="updateBtn" data-id="${
+                         fragment.id
+                       }">Update</button>
+                     </div>`;
         fragmentList.appendChild(li);
       });
     } else {
@@ -58,22 +84,184 @@ async function init() {
 
   await displayFragments(user); // Display user's fragments on page load
 
-  const createForm = document.getElementById("createForm");
   createForm.addEventListener("submit", async (event) => {
     event.preventDefault(); // Prevent form submission
     const fragmentType = document.getElementById("fragmentType").value;
-    const fragmentContent = document.getElementById("fragmentContent").value;
+    const fragmentContentValue =
+      document.getElementById("fragmentContent").value;
+
     try {
-      await createNewFragment(user, fragmentContent, fragmentType); // Create new fragment
+      let res;
+      if (fragmentType.startsWith("image/")) {
+        res = await createNewFragment(
+          user,
+          imageFragment.files[0],
+          fragmentType
+        );
+      } else {
+        res = await createNewFragment(user, fragmentContentValue, fragmentType);
+      }
       await displayFragments(user); // Refresh fragment list
       // Clear the form fields
       document.getElementById("fragmentType").value = "text/plain";
       document.getElementById("fragmentContent").value = "";
+      document.getElementById("imageFragment").value = "";
     } catch (error) {
       console.error("Failed to create fragment:", error);
       alert("Failed to create fragment. Please try again.");
     }
   });
+
+  // View fragment modal
+  fragmentList.addEventListener("click", async (event) => {
+    if (
+      event.target.tagName === "DIV" &&
+      event.target.parentElement.tagName === "LI"
+    ) {
+      const fragmentId = event.target.parentElement
+        .querySelector("div:first-child")
+        .innerText.split(":")[1]
+        .trim();
+      const { data, fragmentType } = await fetchFragmentById(user, fragmentId);
+
+      if (fragmentType.startsWith("image/")) {
+        viewFragmentContent.innerHTML = `<img src="${data}" style="max-width: 100%; height: auto;">`;
+      } else if (fragmentType === "application/json") {
+        viewFragmentContent.textContent = JSON.stringify(data); // Format the JSON data
+      } else {
+        viewFragmentContent.textContent = data;
+      }
+
+      extensionInput.value = fragmentType.split("/")[1];
+      viewModal.style.display = "block";
+      document.querySelectorAll("#fragmentList li").forEach((li) => {
+        li.classList.remove("selected");
+      });
+      event.target.parentElement.classList.add("selected");
+    }
+  });
+
+  // Update fragment modal
+  fragmentList.addEventListener("click", async (event) => {
+    if (event.target.classList.contains("updateBtn")) {
+      const fragmentId = event.target.getAttribute("data-id");
+      const { data, fragmentType } = await fetchFragmentById(user, fragmentId);
+
+      if (fragmentType.startsWith("image/")) {
+        const img = document.createElement("img");
+        img.src = data;
+        img.style.width = "100%";
+        img.id = `${fragmentId}.image`;
+        updateFragmentContent.appendChild(img);
+        updateImageFragment.type = "file";
+        updateImageFragment.accept = "image/*";
+      } else if (fragmentType === "application/json") {
+        updateFragmentContent.value = JSON.stringify(data); // Format the JSON data
+      } else {
+        updateFragmentContent.value = data;
+        updateImageFragment.type = "hidden";
+      }
+
+      updateModal.style.display = "block";
+
+      updateForm.onsubmit = async (evt) => {
+        evt.preventDefault();
+
+        if (fragmentType.startsWith("image/")) {
+          try {
+            await updateFragmentById(
+              user,
+              updateImageFragment.files[0],
+              fragmentId,
+              updateImageFragment.files[0].type
+            );
+            updateImageFragment.value = "";
+          } catch (err) {
+            console.error(err);
+            alert("Failed to update fragment. Please try again.");
+          }
+        } else {
+          const fragmentCurrContent = updateFragmentContent.value;
+          try {
+            await updateFragmentById(
+              user,
+              fragmentCurrContent,
+              fragmentId,
+              fragmentType
+            );
+          } catch (err) {
+            console.error(err);
+            alert("Failed to update fragment. Please try again.");
+          }
+        }
+
+        await displayFragments(user); // Refresh fragment list
+        updateModal.style.display = "none";
+      };
+    }
+  });
+
+  // Delete fragment modal
+  fragmentList.addEventListener("click", async (event) => {
+    if (event.target.classList.contains("deleteBtn")) {
+      const fragmentId = event.target.getAttribute("data-id");
+      confirmDelete.onclick = async () => {
+        try {
+          await deleteFragmentById(user, fragmentId);
+        } catch (err) {
+          console.error(err);
+          alert("Failed to delete fragment. Please try again.");
+        }
+        await displayFragments(user); // Refresh fragment list
+        deleteModal.style.display = "none";
+      };
+      deleteModal.style.display = "block";
+    }
+  });
+
+  // Close modal
+  document.querySelectorAll(".close-btn").forEach((btn) => {
+    btn.onclick = () => {
+      viewModal.style.display = "none";
+      updateModal.style.display = "none";
+      deleteModal.style.display = "none";
+      updateFragmentContent.innerHTML = "";
+    };
+  });
+
+  // Convert fragment
+  convertBtn.onclick = async () => {
+    const extension = extensionInput.value;
+
+    // Get the fragment ID from the clicked fragment in the list
+    const fragmentId = document
+      .querySelector("#fragmentList li.selected")
+      .querySelector("div:first-child")
+      .innerText.split(":")[1]
+      .trim();
+
+    try {
+      const { data } = await fetchConvertedFragmentById(
+        user,
+        fragmentId,
+        extension
+      );
+
+      // Display the converted data in the view modal
+      if (extension.startsWith("image/")) {
+        viewFragmentContent.innerHTML = `<img src="${data}" style="max-width: 100%; height: auto;">`;
+      } else if (fragmentType === "application/json") {
+        viewFragmentContent.textContent = JSON.stringify(data); // Format the JSON data
+      } else {
+        viewFragmentContent.textContent = data;
+      }
+
+      viewModal.style.display = "block";
+    } catch (error) {
+      console.error("Failed to convert fragment:", error);
+      alert(`Failed to convert fragment: ${error.message}`);
+    }
+  };
 }
 
 addEventListener("DOMContentLoaded", init);
